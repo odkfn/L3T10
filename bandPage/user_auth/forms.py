049c89166efb624cff462463ff2e3d6f04d87a7f
odkfn <scott.lynch.06@aberdeen.ac.uk>
2022-10-19 12:21:03 +0100

    Update forms.py
    Added docstrings.

--- bandPage/user_auth/forms.py
+++ bandPage/user_auth/forms.py
@@ -4,6 +4,20 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
+    '''
+    This class creates the SignUpForm.
+
+    The form is used to permit new users to sign up to the website.
+
+    :param str first-name: The users first name
+    :param str username: The username selected by the user
+    :param str password1: The password selected by the user
+    :param str password2: The password selected by the user a second time for verification purposes
+
+    :returns: The title of the post
+    :rtype: str
+    
+    '''
     first_name = forms.CharField(max_length=30, required=True, help_text='Optional.')
 
     class Meta:
