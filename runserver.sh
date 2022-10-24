28e6556a13a10ef9f649fc2a0afe102bf2bcfd13
odkfn <scott.lynch.06@aberdeen.ac.uk>
2022-10-24 12:17:55 +0100

    Docker Functionality
    Made changes required to get docker file to work

--- runserver.sh
+++ runserver.sh
@@ -2,7 +2,7 @@
 
 # to run this file need to use ./runserver.sh as this executable is not in the path
 
-cd '/Users/pinchy/Documents/GitHub/L3T10'
+# cd '/Users/pinchy/Documents/GitHub/L3T10'
 source my_django/bin/activate
 cd bandPage
 python3 manage.py runserver
